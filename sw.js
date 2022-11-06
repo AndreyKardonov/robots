152101df159b8dc30afbf3f53476b919e3c6466d
Andrey <kardona@mail.ru>
2022-11-06 23:14:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","style/style.css"]);
+toolbox.precache(["index.html","game.zip","game/*.*","style/style.css"]);
 
 toolbox.router.get('icons/*', toolbox.cacheFirst);
 
